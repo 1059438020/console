b86120342fbd56442ab5b7d15c45cb193616f91e
1059438020 <1059438020@qq.com>
2019-04-01 19:08:39 +0800

    Initial commit

---
+++ console-spring/src/main/java/org/bzhy/console/comm/CmdSocketCommon.java
@@ -0,0 +1,51 @@
+package org.bzhy.console.comm;
+
+import org.bzhy.console.service.impl.CmdSocketServiceImpl;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+/**
+ * @Author: 白振宇
+ * @Date： 2019/4/1 17:43
+ */
+public class CmdSocketCommon extends Thread {
+    private ServerSocket serverSocket;
+
+    public CmdSocketCommon(Integer port) throws IOException
+    {
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(20000);
+    }
+
+    @Override
+    public void run()
+    {
+        while(true)
+        {
+            try
+            {
+                System.out.println("等待远程连接，端口号为：" + serverSocket.getLocalPort() + "...");
+                Socket server = serverSocket.accept();
+                System.out.println("远程主机地址：" + server.getRemoteSocketAddress());
+                DataInputStream in = new DataInputStream(server.getInputStream());
+                System.out.println(in.readUTF());
+                DataOutputStream out = new DataOutputStream(server.getOutputStream());
+                out.writeUTF("谢谢连接我：" + server.getLocalSocketAddress() + "\nGoodbye!");
+                server.close();
+            }catch(SocketTimeoutException s)
+            {
+                System.out.println("Socket timed out!");
+                break;
+            }catch(IOException e)
+            {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+}
---
+++ console-spring/src/main/java/org/bzhy/console/service/TaskService.java
@@ -0,0 +1,9 @@
+package org.bzhy.console.service;
+
+/**
+ * @Author: 白振宇
+ * @Date： 2019/4/1 17:22
+ */
+public interface TaskService {
+    void cmdSocket(String portStr);
+}
