ff8bdc915a348479a7c89d9c50b8003570de84b1
1059438020 <1059438020@qq.com>
2019-04-07 13:04:24 +0800

    客户端可以监听服务器端传来的消息并且执行相应操作

--- console-app/src/Main.java
+++ console-app/src/Main.java
@@ -1,16 +1,53 @@
-import java.net.*;
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Socket socket = new Socket("127.0.0.1", 8888);
-            InputStream inFromServer = socket.getInputStream();
-            DataInputStream in = new DataInputStream(inFromServer);
-            Runtime.getRuntime().exec("cmd /c "+in.readUTF());
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    static ExecutorService threadPool = Executors.newCachedThreadPool();
+    static class ListenThread extends Thread {
+        @Override
+        public void run() {
+            Socket socket = null;
+            try {
+                socket = new Socket("127.0.0.1", 8888);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while(true) {
+                try {
+                    InputStream inFromServer = socket.getInputStream();
+                    DataInputStream in = new DataInputStream(inFromServer);
+                    String str = in.readUTF();
+                    if(!str.isEmpty()) {
+                        WorkThread workThread = new WorkThread(str);
+                        threadPool.execute(workThread);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+    }
+    static class WorkThread extends Thread {
+        String str;
+        WorkThread(String str) {
+            this.str = str;
+        }
+        @Override
+        public void run() {
+            try {
+                Runtime.getRuntime().exec(str);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        ListenThread listenThread = new ListenThread();
+        threadPool.execute(listenThread);
+
     }
 }
