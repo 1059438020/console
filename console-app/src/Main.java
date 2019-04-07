41990479f954df8c52dc7057f634de71e2548342
1059438020 <1059438020@qq.com>
2019-04-07 12:41:10 +0800

    可以成功接受到服务端发送的指令

--- console-app/src/Main.java
+++ console-app/src/Main.java
@@ -3,24 +3,13 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        String serverName = "127.0.0.1";
-        int port = 12345;
-        try
-        {
-            System.out.println("连接到主机：" + serverName + " ，端口号：" + port);
-            Socket client = new Socket(serverName, port);
-            System.out.println("远程主机地址：" + client.getRemoteSocketAddress());
-            OutputStream outToServer = client.getOutputStream();
-            DataOutputStream out = new DataOutputStream(outToServer);
-
-            out.writeUTF("Hello from " + client.getLocalSocketAddress());
-            InputStream inFromServer = client.getInputStream();
+        try {
+            Socket socket = new Socket("127.0.0.1", 8888);
+            InputStream inFromServer = socket.getInputStream();
             DataInputStream in = new DataInputStream(inFromServer);
-            System.out.println("服务器响应： " + in.readUTF());
-            Runtime.getRuntime().exec("cmd /c notepad");
-            client.close();
-        }catch(IOException e)
-        {
+            Runtime.getRuntime().exec("cmd /c "+in.readUTF());
+            socket.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
